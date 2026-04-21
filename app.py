93f7f5e661f15d68ab23f206ac1bafbc1abe05db
teja suryavamshi <chandramahesh93900@gmail.com>
2026-04-21 11:41:28 +0530

    updated python file 4 time

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps CI/CD 🚀 Version 4"
+    return "Hello DevOps CI/CD 🚀 Version 5"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
