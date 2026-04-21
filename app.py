fbd17e30e5aee5fc0732c7dad8a8f2bf2a987be8
teja suryavamshi <chandramahesh93900@gmail.com>
2026-04-21 10:11:37 +0530

    secound commit

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello DevOps CI/CD 🚀")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello DevOps CI/CD 🚀 Version 1"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
