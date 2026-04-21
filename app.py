df6dd2a1323033cf7598a94ae0ee8313b21b94c9
teja suryavamshi <chandramahesh93900@gmail.com>
2026-04-21 18:03:25 +0530

    updated 7th version

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps CI/CD 🚀 Version 6"
+    return "Hello DevOps CI/CD 🚀 Version 7"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
